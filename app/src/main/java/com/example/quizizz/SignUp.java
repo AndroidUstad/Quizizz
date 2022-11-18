d055399d974abc713e3b74d57d907ed07900992b
umair <umair@umairs-MacBook-Pro.local>
2022-11-18 23:58:35 +0500

    Initial commit

---
+++ app/src/main/java/com/example/quizizz/SignUp.java
@@ -0,0 +1,92 @@
+package com.example.quizizz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.quizizz.databinding.ActivitySignUpBinding;
+
+
+import com.example.quizizz.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class SignUp extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+    FirebaseAuth auth;
+    FirebaseFirestore database;
+    ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseFirestore.getInstance();
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("We're creating new account...");
+
+        binding.createNewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, pass, name, referCode;
+
+                email = binding.emailBox.getText().toString();
+                pass = binding.passwordBox.getText().toString();
+                name = binding.nameBox.getText().toString();
+                referCode = binding.referBox.getText().toString();
+                final User user = new User(name, email, pass, referCode);
+
+                dialog.show();
+
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                     if (task.isSuccessful()){
+                         String uid = task.getResult().getUser().getUid();
+                         database
+                                 .collection("users")
+                                 .document(uid)
+                                 .set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                     @Override
+                                     public void onComplete(@NonNull Task<Void> task) {
+                                         if(task.isSuccessful()) {
+                                             dialog.dismiss();
+                                             startActivity(new Intent(SignUp.this, MainActivity.class));
+                                             finish();
+                                         } else {
+                                             dialog.dismiss();
+                                             Toast.makeText(SignUp.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                         }
+                                     }
+                                 });
+                     }  else {
+                         dialog.dismiss();
+                         Toast.makeText(SignUp.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                     }
+                    }
+                });
+            }
+        });
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUp.this, Login.class));
+            }
+        });
+
+    }
+}
